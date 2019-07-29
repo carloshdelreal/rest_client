3b00c2ecbc94a3e548145f96c644923333e328a9
trox115 <tofernandes67@gmail.com>
2019-07-29 21:09:50 +0100

    Added search to bing, with regex

--- rest-cli.rb
+++ rest-cli.rb
@@ -23,3 +23,27 @@ raw = RestClient::Request.execute(
     method: :get,
     url: 'http://releases.ubuntu.com/16.04.2/ubuntu-16.04.2-desktop-amd64.iso',
     raw_response: true)
+
+
+    def bing_search (array,expression)
+        puts "hello"
+        url='https://www.bing.com'
+        search = RestClient.get url, {params: {q: array}}
+        res= []
+        search.body.scan(expression) {res << $~}
+        i=0
+        puts "Results"
+        res.each do |x|
+            puts x 
+        end
+    
+    end
+
+regex = /<h2><a.*?href="(?<link>.*?)".*?h=.*?>(?<text>.*?)<\/a.*?>/
+
+puts "welcome to bing"
+
+puts "what do you want to search?"
+array=gets.chomp
+
+bing_search(array,regex)
